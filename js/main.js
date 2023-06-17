c41d4bb6578f14082eb74642bb7187a8ccec5ac1
João Diniz <joaodiniz@msn.com>
2023-06-17 20:22:14 -0300

    Now it shows video too

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@ const description = document.getElementById("description");
 const explanation = document.getElementById("explanation");
 const title = document.getElementById("title");
 const date = document.getElementById("date");
+const video = document.getElementById("video");
 
 async function logJSONData() {
     const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=EUhzjha07LLtLmnzLdYkD6JV9eO8vx3IefV1SNfE");
     const jsonData = await response.json();
 
-    mainPhoto.setAttribute("src", jsonData.hdurl);
+    // Check media type(Video or Image)
+    if (jsonData.media_type == "video") {
+        video.setAttribute("src", jsonData.url)
+        video.setAttribute("style", "display:block");
+    }else {
+        mainPhoto.setAttribute("src", jsonData.hdurl);
+    }
     title.textContent = jsonData.title;
     date.textContent = jsonData.date;
     explanation.textContent = jsonData.explanation;
